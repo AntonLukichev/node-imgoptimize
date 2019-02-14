ff86fec3888e6f6145168d0332448ff7e8268302
AntonLukichev <anton@lukichev.pro>
2019-02-14 19:53:09 +0300

    add fastify server

---
+++ app.js
@@ -0,0 +1,36 @@
+const fastify = require('fastify')({
+  logger: true
+});
+const axios = require('axios');
+const sharp = require('sharp');
+//  const querystring = require('querystring');
+//  const fs = require('fs');
+const CONFIG = require('./config');
+
+fastify.register(require('fastify-url-data'), (err) => {
+  if (err) throw err
+});
+
+fastify.get('/', async (req, rep) => {
+  const urlData = req.urlData();
+  req.log.info(urlData.path);
+  req.log.info(urlData.query);
+
+  return { hello: 'world' }
+});
+
+const start = async () => {
+  try {
+    await fastify.listen(CONFIG.httpPort, (err, address)=>{
+      if (err) {
+        fastify.log.error(err);
+        process.exit(1)
+      }
+    })
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1)
+  }
+};
+
+start();
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  httpHost: '',
+  httpPort: 3000
+
+}
